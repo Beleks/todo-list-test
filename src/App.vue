149d5fe38b15e92a941595d128c5d8b69041f0ac
Aleksey Beletsky <53120159+Beleks@users.noreply.github.com>
2025-04-13 14:03:25 +0800

    Use RouterLink instead of div
    - Изменение стилей активной вкладки в меню

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,30 @@
 <script setup>
-import {RouterLink, RouterView, useRouter} from 'vue-router'
+import { useRouter } from 'vue-router'
 
-const menuItems = ['taskList', 'settings', 'about']
-const router = useRouter()
-
-function choosePage(item) {
-  router.replace({name: item})
-}
+const menuItems = [
+  { title: 'Список задач', routeName: 'taskList' },
+  { title: 'Настройки', routeName: 'settings' },
+  { title: 'О проекте', routeName: 'about' },
+]
 </script>
 
 <template>
-  <div class="flex justify-center gap-6">
-    <div v-for="item in menuItems" @click="choosePage(item)">
-      {{ item }}
-    </div>
+  <div class="flex justify-center gap-6 text-gray-400">
+    <RouterLink
+      replace
+      v-for="item in menuItems"
+      :to="{ name: item.routeName }"
+      class="font-sans font-medium"
+      activeClass="text-gray-50"
+    >
+      {{ item.title }}
+    </RouterLink>
   </div>
 
   <RouterView />
 </template>
 
 <style scoped>
-
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
