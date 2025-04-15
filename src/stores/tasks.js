5d5189635d1e5fee9515bdac2f8104ae6bfa0d3e
Aleksey Beletsky <53120159+Beleks@users.noreply.github.com>
2025-04-15 13:58:52 +0800

    Remove less methods from store

--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -41,52 +41,11 @@ export const useTasksStore = defineStore('tasks', () => {
     task.isReady = true
   }
 
-  const taskPriorities = ref({
-    1: {
-      title: 'Низкий',
-      class: 'stroke-blue-400',
-      activeClass: 'text-blue-400',
-    },
-    2: {
-      title: 'Средний',
-      class: 'stroke-yellow-400',
-      activeClass: 'text-yellow-400',
-    },
-    3: {
-      title: 'Высокий',
-      class: 'stroke-red-400',
-      activeClass: 'text-red-400',
-    },
-  })
-
-  const statusTypes = ref({
-    all: {
-      title: 'Все задачи',
-      checkQuality() {
-        return true
-      },
-    },
-    active: {
-      title: 'Активные',
-      checkQuality(task) {
-        return !task.isReady
-      },
-    },
-    completed: {
-      title: 'Завершенные',
-      checkQuality(task) {
-        return task.isReady
-      },
-    },
-  })
-
   return {
     allTasks,
     createTask,
     deleteTaskById,
     saveEditTask,
     completeTask,
-    taskPriorities,
-    statusTypes,
   }
 })
